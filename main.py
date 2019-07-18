dd86c28a036a0a161bdb748d442c255f4e9845f0
Eric Chi <ericjaychi@gmail.com>
2019-07-18 16:14:45 -0400

    GH-6 Display what sets the card has been printed in

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 import fire
 import requests
 
+DATA = "data"
+NAME = "name"
+PRINTS_SEARCH_URI = "prints_search_uri"
+SET_NAME = "set_name"
+
 
 class Card:
 
@@ -10,7 +15,27 @@ class Card:
         # TODO: Need to figure out how to deal with spaces.
         response = requests.get("https://api.scryfall.com/cards/named?fuzzy=%s" % name)
 
-        return response.json()['name']
+        # TODO: Maybe consider refactoring the print items into a separate method or at least pretty it up with tabs.
+        print(Card.__get_card_name(response))
+
+        for set_name in Card.__get_card_set_names(response):
+            print("\t" + set_name)
+
+    @staticmethod
+    def __get_card_name(response):
+        return response.json()[NAME]
+
+    @staticmethod
+    def __get_card_set_names(response):
+        card_set_list = []
+        prints_search_uri = response.json()[PRINTS_SEARCH_URI]
+
+        card_sets_response = requests.get(prints_search_uri)
+
+        for json in card_sets_response.json()[DATA]:
+            card_set_list.append(json[SET_NAME])
+
+        return card_set_list
 
 
 if __name__ == '__main__':
