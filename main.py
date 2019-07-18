b753bf4aae80a479ad506e9b7933b1dd159f27b0
Eric Chi <ericjaychi@gmail.com>
2019-07-18 15:03:16 -0400

    GH-4 Allow dynamic searching for a card name

--- main.py
+++ main.py
@@ -1,46 +1,17 @@
-import cv2
+#!/usr/bin/env python
+import fire
 import requests
-import shutil
 
 
-def main():
-    get_card()
-    apply_threshold()
+class Card:
+
+    @staticmethod
+    def search(name):
+        # TODO: Need to figure out how to deal with spaces.
+        response = requests.get("https://api.scryfall.com/cards/named?fuzzy=%s" % name)
+
+        return response.json()['name']
 
 
-def get_card():
-
-    url = "https://api.scryfall.com/cards/named?exact=watery+grave&format=image"
-
-    response = requests.get(url, stream=True)
-
-    # TODO: Need to create a constant for the images
-    # TODO: Might need to get the card name or something so that it can added into
-    # Opens a file with the image name. Has write access and binary access.
-    with open("images/img.jpg", "wb") as output_file:
-        shutil.copyfileobj(response.raw, output_file)
-    del response
-
-
-def apply_threshold():
-    # Load an color image in grayscale
-    original = cv2.imread("images/img.jpg")
-    greyscale = cv2.imread("images/img.jpg", 0)
-
-    # Is pretty good for stock images from the looks of it.
-    retval, threshold = cv2.threshold(greyscale, 125, 255, cv2.THRESH_BINARY)
-
-    # Might be better for worse lighting, but for stock images, its pretty bad.
-    gaussian = cv2.adaptiveThreshold(greyscale, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
-
-    cv2.imshow("original", original)
-    cv2.imshow("greyscale", greyscale)
-
-    cv2.imshow("threshold", threshold)
-    cv2.imshow("gaussian", gaussian)
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-main()
+if __name__ == '__main__':
+    fire.Fire(Card)
