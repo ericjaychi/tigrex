dc264cb7a17f1ee9de885741460a5001066b925c
Eric Chi <ericjaychi@gmail.com>
2019-07-18 18:23:32 -0400

    GH-6 Create basic methods that return normal and foil prices

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import fire
 import requests
 
 DATA = "data"
+DOLLAR_SIGN = "$"
+FORWARD_SLASH = "/"
+FUZZY_SEARCH = "https://api.scryfall.com/cards/named?fuzzy=%s"
 NAME = "name"
+PRICES = "prices"
 PRINTS_SEARCH_URI = "prints_search_uri"
 SET_NAME = "set_name"
+SPACE = " "
+TAB = "\t"
+USD = "usd"
+USD_FOIL = "usd_foil"
 
 
 class Card:
@@ -13,13 +21,17 @@ class Card:
     @staticmethod
     def search(name):
         # TODO: Need to figure out how to deal with spaces.
-        response = requests.get("https://api.scryfall.com/cards/named?fuzzy=%s" % name)
+        response = requests.get(FUZZY_SEARCH % name)
 
-        # TODO: Maybe consider refactoring the print items into a separate method or at least pretty it up with tabs.
+        # TODO: Refactor all the print statements into a separate method.
         print(Card.__get_card_name(response))
 
+        card_price_usd = TAB + TAB + DOLLAR_SIGN + Card.__get_card_prices_usd(response) + \
+                         SPACE + FORWARD_SLASH + SPACE + DOLLAR_SIGN + Card.__get_card_prices_usd_foil(response)
+
         for set_name in Card.__get_card_set_names(response):
-            print("\t" + set_name)
+            print(TAB + set_name)
+            print(card_price_usd)
 
     @staticmethod
     def __get_card_name(response):
@@ -36,6 +48,15 @@ class Card:
             card_set_list.append(json[SET_NAME])
 
         return card_set_list
+
+    # TODO: This will need to account for each individual price of the card from whichever set its from.
+    @staticmethod
+    def __get_card_prices_usd(response):
+        return response.json()[PRICES][USD]
+
+    @staticmethod
+    def __get_card_prices_usd_foil(response):
+        return response.json()[PRICES][USD_FOIL]
 
 
 if __name__ == '__main__':
