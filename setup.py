32253b657eeb8fcf9b3ec8e77df29144c6bb68f5
Eric Chi <ericjaychi@gmail.com>
2019-07-19 01:38:20 -0400

    GH-5 Publish package to PyPi

--- setup.py
+++ setup.py
@@ -3,22 +3,21 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+requirements = ["requests<=2.21.0", "fire<=0.1.3"]
+
 setuptools.setup(
     name="tigrex",
-    version="1.0.0",
+    version="1.0.3",
     author="Eric Chi",
     author_email="ericjaychi@gmail.com",
     description="A Magic the Gathering CLI Tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        "fire",
-        "requests"
     ]
 )
