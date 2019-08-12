18a0a4f70d0e72c3c4ee840810f5efefa37d2e73
chris <chris@wizard>
2019-08-12 11:24:01 +0200

    Added functionality to change to eur or tix as currency, added config file aswell

--- tigrex/__main__.py
+++ tigrex/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import fire
 import requests
+import configparser
 
 CARD_FACES = "card_faces"
 CREATURE = "Creature"
@@ -35,8 +36,10 @@ TOUGHNESS = "toughness"
 TRANSFORM = "transform"
 TRANSFORM_LINE_BREAK = "---------"
 TYPE_LINE = "type_line"
-USD = "usd"
 USD_FOIL = "usd_foil"
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 
 class Card:
@@ -62,39 +65,60 @@ class Card:
         Card.__print_card_search(response, card_layout)
 
     @staticmethod
+    def currency(*args):
+        if not len(args) == 1:
+            return "Please only provide one value for the currency"
+        if args[0] == 'eur':
+            config['Price']['sign'] = '€'
+            config['Price']['Cur'] = str(args[0])
+        elif args[0] == 'usd':
+            config['Price']['sign'] = '$'
+            config['Price']['Cur'] = str(args[0])
+        elif args[0] == 'tix':
+            config['Price']['sign'] = 'tix '
+            config['Price']['Cur'] = str(args[0])
+        else:
+            return "Please only provide, usd, eur or tix as currency options"
+
+        with open('config.ini', "w") as configfile:
+            config.write(configfile)
+
+    @staticmethod
     def __get_card_description(response, card_layout):
         description = EMPTY_STRING
 
         if card_layout == NORMAL or card_layout == MELD or card_layout == SAGA:
             if CREATURE in response.json()[TYPE_LINE]:
                 description = response.json()[TYPE_LINE] + NEW_LINE + NEW_LINE + \
-                              response.json()[ORACLE_TEXT] + NEW_LINE + NEW_LINE + \
-                              response.json()[POWER] + SPACE + FORWARD_SLASH + SPACE + response.json()[TOUGHNESS]
+                    response.json()[ORACLE_TEXT] + NEW_LINE + NEW_LINE + \
+                    response.json()[POWER] + SPACE + FORWARD_SLASH + \
+                    SPACE + response.json()[TOUGHNESS]
             else:
                 description = response.json()[TYPE_LINE] + NEW_LINE + NEW_LINE + \
-                              response.json()[ORACLE_TEXT]
+                    response.json()[ORACLE_TEXT]
         elif card_layout == TRANSFORM or card_layout == SPLIT or card_layout == FLIP:
             if CREATURE in response.json()[TYPE_LINE]:
                 front_description = response.json()[CARD_FACES][0][TYPE_LINE] + NEW_LINE + NEW_LINE + \
-                                    response.json()[CARD_FACES][0][ORACLE_TEXT] + NEW_LINE + NEW_LINE + \
-                                    response.json()[CARD_FACES][0][POWER] + SPACE + FORWARD_SLASH + SPACE + \
-                                    response.json()[CARD_FACES][0][TOUGHNESS]
+                    response.json()[CARD_FACES][0][ORACLE_TEXT] + NEW_LINE + NEW_LINE + \
+                    response.json()[CARD_FACES][0][POWER] + SPACE + FORWARD_SLASH + SPACE + \
+                    response.json()[CARD_FACES][0][TOUGHNESS]
 
                 if PLANESWALKER in response.json()[CARD_FACES][1][TYPE_LINE]:
                     back_description = response.json()[CARD_FACES][1][TYPE_LINE] + NEW_LINE + NEW_LINE + \
-                                       response.json()[CARD_FACES][1][ORACLE_TEXT]
+                        response.json()[CARD_FACES][1][ORACLE_TEXT]
                 else:
                     back_description = response.json()[CARD_FACES][1][TYPE_LINE] + NEW_LINE + NEW_LINE + \
-                                       response.json()[CARD_FACES][1][ORACLE_TEXT] + NEW_LINE + NEW_LINE + \
-                                       response.json()[CARD_FACES][1][POWER] + SPACE + FORWARD_SLASH + SPACE + \
-                                       response.json()[CARD_FACES][1][TOUGHNESS]
+                        response.json()[CARD_FACES][1][ORACLE_TEXT] + NEW_LINE + NEW_LINE + \
+                        response.json()[CARD_FACES][1][POWER] + SPACE + FORWARD_SLASH + SPACE + \
+                        response.json()[CARD_FACES][1][TOUGHNESS]
             else:
                 front_description = response.json()[CARD_FACES][0][TYPE_LINE] + NEW_LINE + NEW_LINE + \
-                                    response.json()[CARD_FACES][0][ORACLE_TEXT]
+                    response.json()[CARD_FACES][0][ORACLE_TEXT]
                 back_description = response.json()[CARD_FACES][1][TYPE_LINE] + NEW_LINE + NEW_LINE + \
-                                   response.json()[CARD_FACES][1][ORACLE_TEXT]
+                    response.json()[CARD_FACES][1][ORACLE_TEXT]
 
-            description = front_description + NEW_LINE + TRANSFORM_LINE_BREAK + NEW_LINE + back_description
+            description = front_description + NEW_LINE + \
+                TRANSFORM_LINE_BREAK + NEW_LINE + back_description
 
         return description
 
@@ -112,7 +136,8 @@ class Card:
             mana_cost = response.json()[CARD_FACES][0][MANA_COST]
         elif card_layout == SPLIT:
             mana_cost = response.json()[CARD_FACES][0][MANA_COST] + \
-                        SPACE + FORWARD_SLASH + FORWARD_SLASH + SPACE + response.json()[CARD_FACES][1][MANA_COST]
+                SPACE + FORWARD_SLASH + FORWARD_SLASH + SPACE + \
+                response.json()[CARD_FACES][1][MANA_COST]
 
         return mana_cost
 
@@ -131,7 +156,8 @@ class Card:
         for json in card_sets_response.json()[DATA]:
             set_name = json[SET_NAME]
             set_code = json[SET].upper()
-            normal_price = json[PRICES][USD]
+
+            normal_price = json[PRICES][config['Price']['cur']]
             foil_price = json[PRICES][USD_FOIL]
 
             set_name = set_name + SPACE + PARENTHESIS_LEFT + set_code + PARENTHESIS_RIGHT
@@ -154,7 +180,8 @@ class Card:
 
     @staticmethod
     def __print_card_header(response, card_layout):
-        print(Card.__get_card_name(response) + TAB + TAB + Card.__get_card_mana_cost(response, card_layout) + NEW_LINE)
+        print(Card.__get_card_name(response) + TAB + TAB +
+              Card.__get_card_mana_cost(response, card_layout) + NEW_LINE)
 
     @staticmethod
     def __print_card_price(response):
@@ -172,7 +199,8 @@ class Card:
             else:
                 foil_price = NOT_AVAILABLE
 
-            print(TAB + TAB + DOLLAR_SIGN + normal_price + SPACE + FORWARD_SLASH + SPACE + DOLLAR_SIGN + foil_price)
+            print(TAB + TAB + config['Price']['sign'] + normal_price + SPACE +
+                  FORWARD_SLASH + SPACE + DOLLAR_SIGN + foil_price)
 
     @staticmethod
     def __print_card_search(response, card_layout):
